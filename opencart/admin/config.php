83d95c50c4ee2c040c087be9a0e533cced9d7482
root <root@gandalf.(none)>
2013-01-01 09:44:17 -0200

    OpenCart instalado

---
+++ opencart/admin/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.brinquedosetravessuras.com.br/opencart/admin/');
+define('HTTP_CATALOG', 'http://www.brinquedosetravessuras.com.br/opencart/');
+define('HTTP_IMAGE', 'http://www.brinquedosetravessuras.com.br/opencart/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.brinquedosetravessuras.com.br/opencart/admin/');
+define('HTTPS_CATALOG', 'http://www.brinquedosetravessuras.com.br/opencart/');
+define('HTTPS_IMAGE', 'http://www.brinquedosetravessuras.com.br/opencart/image/');
+
+// DIR
+define('DIR_APPLICATION', '/home/www/opencart/admin/');
+define('DIR_SYSTEM', '/home/www/opencart/system/');
+define('DIR_DATABASE', '/home/www/opencart/system/database/');
+define('DIR_LANGUAGE', '/home/www/opencart/admin/language/');
+define('DIR_TEMPLATE', '/home/www/opencart/admin/view/template/');
+define('DIR_CONFIG', '/home/www/opencart/system/config/');
+define('DIR_IMAGE', '/home/www/opencart/image/');
+define('DIR_CACHE', '/home/www/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/home/www/opencart/download/');
+define('DIR_LOGS', '/home/www/opencart/system/logs/');
+define('DIR_CATALOG', '/home/www/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart');
+define('DB_PASSWORD', 'tqbfjotld');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', '');
+?>
